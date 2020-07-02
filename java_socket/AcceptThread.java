754187fe258ab031bef553a98615f3b767af52b3
source <ksyee01290@naver.com>
2020-07-02 16:21:56 +0900

    다중접속이가능한 서버

--- java_socket/AcceptThread.java
+++ java_socket/AcceptThread.java
@@ -1,6 +1,9 @@
 package java_socket;
 import java.net.Socket;
 import java.net.ServerSocket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class AcceptThread implements Runnable {
     ServerSocket serverSocket;
@@ -14,11 +17,39 @@ public class AcceptThread implements Runnable {
         while(true){
             try{
                 client = serverSocket.accept();
+                new Receiver(client).start();
                 System.out.println("connected client"+client);
+                
             }catch(Exception e){
 
             }
         }
     }
 
+}
+class Receiver extends Thread{
+    Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+
+    Receiver(Socket socket){
+        this.socket = socket;
+        try{
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+        }catch(IOException e){}
+    }
+
+    public void run(){
+        while(in!=null){
+         
+            try{
+                int source = in.readInt();
+                System.out.println(source);
+
+                out.writeInt(source+1);
+
+            }catch(IOException e){}
+        }
+    }
 }
